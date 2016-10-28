83b2b94200f09aa1766bb974e27c2531c6497c65
Ben Morton <morton@roe.ac.uk>
2016-10-28 17:24:51 +0100

    end 28/10/2016

--- signal_processing.py
+++ signal_processing.py
@@ -5,12 +5,15 @@ signal = np.loadtxt("signal.dat")
 
 length = len(signal/2)
 
-time = np.zeros(shape=(length))
-count = np.zeros(shape=(length))
+time = signal[:,0]
+count = signal[:,1]
 
-for i in range(length):
-    time[i] = signal[i,0]
-    count[i] = signal[i,1]
+#time = np.zeros(shape=(length))
+#count = np.zeros(shape=(length))
+
+#for i in range(length):
+#    time[i] = signal[i,0]
+#    count[i] = signal[i,1]
 
 plt.figure(1)
 
@@ -19,20 +22,90 @@ plt.plot(time,count)
 plt.xlabel("Time [s]")
 plt.ylabel("Count [1/s]")
 
-
 fft_signal = np.fft.fft(count)
 
 freq = np.fft.fftfreq(length)
 
-#print freq
-
-fft_signal=fft_signal+1
-
-y = np.log10(fft_signal)
+power=(abs(fft_signal))/length
 
 plt.subplot(212)
-plt.plot(freq,y.real)
-plt.xlabel("Frequency [1/s]")
-plt.ylabel("Counts [1/s]")
+plt.semilogy(freq[0:500],power[0:500])
+plt.semilogy(freq[501:1000],power[501:1000],color="b")
+plt.xlabel("k [Hz]")
+plt.ylabel("Power")
+
+n_random=10000
+n_points=1000
+
+n_bins=500
+
+numb=np.zeros(shape=(n_bins))
+numb_vals=np.zeros(shape=(n_bins))
+
+max_signal=5
+
+for i in range(n_random):
+    rand_signal = np.sqrt(power.real[0])*np.random.normal(0,1,(n_points))+power[0]
+    fft_rand_signal=np.fft.fft(rand_signal)
+    power_rand_signal=abs(fft_rand_signal)/length
+    power_rand_signal[0]=0.0
+    for j in range(n_points):
+        numb_bin =int((power_rand_signal[j]/max_signal)*float(n_bins))
+        numb[numb_bin]=numb[numb_bin]+1
+
+sum_numb = np.sum(numb)
+        
+for i in range(n_bins):
+    numb_vals[i]=max_signal*float(i)/float(n_bins)
+    numb[i]=float(numb[i])/sum_numb
+
+plt.figure(2)
+plt.subplot(211)
+plt.plot(numb_vals,numb)
+plt.xlabel("Power")
+plt.ylabel("Probability")
+
+cum_numb=np.zeros(shape=(n_bins))
+
+found=0
+
+for i in range(n_bins):
+    cum_numb[i]=np.sum(numb[0:i])
+    if cum_numb[i]>=0.9995 and found==0:
+        found=1
+        print "95% confidence is", (numb_vals[i-1]+numb_vals[i])/2.0
+        ninefive_conf=(numb_vals[i-1]+numb_vals[i])/2.0
+
+plt.subplot(212)
+plt.plot(numb_vals,cum_numb)
+plt.xlabel("Power")
+plt.ylabel("Cumulative Probability")
+
+x = np.linspace(-1,max_signal)
+y = (x/x)*0.9995
+
+y3 = np.linspace(-1,1)
+x3 = (y/y)*ninefive_conf
+    
+plt.figure(3)
+
+plt.subplot(211)
+plt.plot(numb_vals,cum_numb)
+plt.plot(x,y)
+plt.plot(x3,y3,color="g")
+plt.xlim(2,3)
+plt.ylim(0.995,1.0)
+plt.xlabel("Power")
+plt.ylabel("Cumulative Probability")
+y5 = np.linspace(-1,1)
+
+plt.subplot(212)
+plt.semilogy(freq[0:500],power[0:500])
+plt.semilogy(freq[501:1000],power[501:1000],color="b")
+plt.plot(y5,x3)
+plt.xlabel("k [Hz]")
+plt.ylabel("Power")
+plt.xlim(-0.5,0.5)
 
 plt.show()
+
